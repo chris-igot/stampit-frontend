e39c4bd5550886f15dbe6de265ec18f1f1a60f39
cigot <c.bensi.igot@gmail.com>
2022-01-02 02:17:21 -0500

    Added Sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import Menu from "./components/menu";
 
 function App() {
