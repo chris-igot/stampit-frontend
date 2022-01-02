e5903bd7a20831b7d2b8205e128c7dcbe3fccda7
cigot <c.bensi.igot@gmail.com>
2022-01-02 11:32:18 -0500

    Housekeeping -changed locations of forms -updated routes -separated the menu -added a .env file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.scss";
-import Menu from "./components/menu";
+import Menu from "./menu";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Profile from "./pages/profile";
+import PostNew from "./pages/postNew";
+import Public from "./pages/public";
 
 function App() {
+    useEffect(() => {
+        console.log((process.env as any).REACT_APP_SERVER_PATH);
+    }, []);
     return (
         <Router>
             <Menu />
             <Routes>
-                <Route path="/fetchtest" element={<div></div>}></Route>
-                <Route path="/profile" element={<div></div>}></Route>
-                <Route path="/upload" element={<div></div>}></Route>
+                <Route path="/home" element={<Profile home={true} />} />
+                <Route path="/profile" element={<Profile home={false} />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/public" element={<Public />} />
+                <Route path="/profile" element={<div></div>} />
+                <Route path="/upload" element={<PostNew />} />
             </Routes>
         </Router>
     );
---
+++ src/menu.tsx
@@ -0,0 +1,30 @@
+import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import getData from "./utilities/getData";
+interface PropsType {
+    children?: React.ReactNode;
+}
+function Menu(props: PropsType) {
+    const navigate = useNavigate();
+    return (
+        <nav>
+            <Link to="/home">home </Link>
+            <Link to="/login">login </Link>
+            <Link to="/register">register </Link>
+            <Link to="/public">public </Link>
+            <Link to="/profile">profile</Link>
+            <Link to="/upload">upload</Link>
+            <Link
+                to="/login"
+                onClick={() => {
+                    fetch("/api/logout");
+                }}
+            >
+                logout
+            </Link>
+            {props.children}
+        </nav>
+    );
+}
+
+export default Menu;
