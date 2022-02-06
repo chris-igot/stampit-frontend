88c28fe60c3b9e79e547828907c2494ec0714676
cigot <c.bensi.igot@gmail.com>
2022-02-06 02:45:24 -0500

    Added onChange callback

--- src/components/form/fileInput.tsx
+++ src/components/form/fileInput.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 
+type onChangeFunc = (e: React.ChangeEvent<HTMLInputElement>) => void;
+
 interface PropsType {
     name: string;
     multiple?: boolean;
+    onChange?: onChangeFunc;
 }
 
 export default function FileInput(props: PropsType) {
     return (
         <div className="form__input-file">
             {props.multiple === true ? (
-                <input type="file" name={props.name} id={props.name} multiple />
+                <input
+                    type="file"
+                    name={props.name}
+                    id={props.name}
+                    multiple
+                    onChange={(e) => {
+                        if ("onChange" in props) {
+                            (props.onChange as onChangeFunc)(e);
+                        }
+                    }}
+                />
             ) : (
-                <input type="file" name={props.name} id={props.name} />
+                <input
+                    type="file"
+                    name={props.name}
+                    id={props.name}
+                    onChange={(e) => {
+                        if ("onChange" in props) {
+                            (props.onChange as onChangeFunc)(e);
+                        }
+                    }}
+                />
             )}
         </div>
     );
