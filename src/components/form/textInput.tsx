3e02ceab0b597bf39dac4005cbe5e2f869622f37
cigot <c.bensi.igot@gmail.com>
2022-02-09 15:02:42 -0500

    Cleaned up code

--- src/components/form/textInput.tsx
+++ src/components/form/textInput.tsx
@@ -1,35 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+type onChangeFunc = (e: React.ChangeEvent<HTMLInputElement>) => void;
 interface PropsType {
     name: string;
     type?: "text" | "email" | "number" | "password" | "search" | "tel" | "url";
     label?: string;
+    value?: string;
+    onChange?: onChangeFunc;
 }
 export default function InputText(props: PropsType) {
     const [blank, setBlank] = useState(true);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (inputRef.current?.value === "") {
+            setBlank(true);
+        } else {
+            setBlank(false);
+        }
+    }, []);
+
     function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
         const str = e.currentTarget.value;
-        console.log(str);
+
         setBlank(str === "");
-        if (str === "") {
-            console.log("blank");
-        } else {
-            console.log("not blank");
-        }
     }
-    useEffect(() => {
-        console.log("input loaded", { ...props });
-    }, []);
+
     return (
         <div
             className={blank ? "form__input-text" : "form__input-text notblank"}
         >
             <input
+                id={props.name}
+                ref={inputRef}
                 type={"type" in props ? props.type : "text"}
                 name={props.name}
-                id={props.name}
-                onKeyUp={(e) => {
-                    handleKeyPress(e);
-                }}
+                defaultValue={"value" in props ? props.value : ""}
+                onKeyUp={handleKeyPress}
             />
             <label htmlFor={props.name}>
                 {"label" in props ? props.label : props.name}
