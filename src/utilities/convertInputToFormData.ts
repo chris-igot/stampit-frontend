1c661cec00a2ac88d95fe8a65f2f2e6216395ad0
cigot <c.bensi.igot@gmail.com>
2022-02-09 01:05:57 -0500

    Modified to be called from a form element

--- src/utilities/convertInputToFormData.ts
+++ src/utilities/convertInputToFormData.ts
@@ -1,21 +1,20 @@
 export default function convertInputToFormData(
-    e: React.MouseEvent<HTMLButtonElement>
+    e: React.FormEvent<HTMLFormElement>
 ): FormData {
-    const inputs = e.currentTarget.parentNode?.querySelectorAll(
+    const inputs = e.currentTarget.querySelectorAll(
         "input,textarea"
     ) as unknown as HTMLInputElement[];
 
     let data = new FormData();
 
     inputs.forEach((input) => {
-        console.log(input.name, input.value);
         if (input.type === "file") {
             const fileList: FileList = input.files as FileList;
 
             if (input.hasAttribute("multiple")) {
                 for (let i = 0; i < fileList.length; i++) {
                     const file = fileList[i];
-                    data.append(input.name + "[]", file, file.name);
+                    data.append(input.name, file, file.name);
                 }
             } else {
                 data.append(input.name, fileList[0]);
