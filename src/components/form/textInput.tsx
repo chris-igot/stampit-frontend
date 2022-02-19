382c747ca4354b2dd09bb31105948ac0b02dd937
cigot <c.bensi.igot@gmail.com>
2022-02-19 15:11:13 -0500

    Modified text input width to be dynamic

--- src/components/form/textInput.tsx
+++ src/components/form/textInput.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useRef, useState } from "react";
 
 type onChangeFunc = (e: React.ChangeEvent<HTMLInputElement>) => void;
+type onKeyUpFunc = (e: React.KeyboardEvent<HTMLInputElement>) => void;
 interface PropsType {
     name: string;
     type?: "text" | "email" | "number" | "password" | "search" | "tel" | "url";
+    width?: string;
+    className?: string;
+    pattern?: string;
+    title?: string;
     label?: string;
     value?: string;
+    onKeyUp?: onKeyUpFunc;
     onChange?: onChangeFunc;
 }
 export default function InputText(props: PropsType) {
     const [blank, setBlank] = useState(true);
+    const divRef = useRef<HTMLDivElement>(null);
     const inputRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
@@ -18,23 +25,48 @@ export default function InputText(props: PropsType) {
         } else {
             setBlank(false);
         }
+
+        if ("width" in props) {
+            (divRef.current as HTMLDivElement).style.width =
+                props.width as string;
+            (inputRef.current as HTMLInputElement).style.width =
+                props.width as string;
+        } else {
+            (divRef.current as HTMLDivElement).style.width =
+                (inputRef.current as HTMLInputElement).clientWidth + "px";
+        }
+        (divRef.current as HTMLDivElement).style.height =
+            (inputRef.current as HTMLInputElement).clientHeight + "px";
     }, []);
 
     function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
         const str = e.currentTarget.value;
 
         setBlank(str === "");
+
+        if ("onKeyUp" in props) {
+            (props.onKeyUp as onKeyUpFunc)(e);
+        }
     }
 
     return (
         <div
-            className={blank ? "form__input-text" : "form__input-text notblank"}
+            className={
+                blank ? "form__input--text" : "form__input--text notblank"
+            }
+            ref={divRef}
         >
             <input
                 id={props.name}
+                className={
+                    "rounded--15 outline--gray interactive" +
+                    ("className" in props ? " " + props.className : "")
+                }
                 ref={inputRef}
                 type={"type" in props ? props.type : "text"}
                 name={props.name}
+                pattern={"pattern" in props ? props.pattern : ".?"}
+                title={"title" in props ? props.title : ""}
                 defaultValue={"value" in props ? props.value : ""}
                 onKeyUp={handleKeyPress}
             />
