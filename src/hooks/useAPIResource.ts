74d40f8d1e2ccd35d1506155de0dca7decd4e4db
cigot <c.bensi.igot@gmail.com>
2022-02-27 16:07:51 -0500

    Added custom hook for api resource access

--- src/hooks/useAPIResource.ts
+++ src/hooks/useAPIResource.ts
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 interface OutputType<T> {
     status: number;
-    json: T;
+    value: T;
     error?: string;
 }
 
@@ -14,11 +14,14 @@ interface StatusResponseFunctions {
 
 export default function useAPIResource<T>(
     apiPath: PathFunctionType,
-    fetchInit: RequestInit = { method: "GET" }
+    defaultValue: T,
+    responseFunctions: StatusResponseFunctions = {},
+    fetchInit: RequestInit = { method: "GET" },
+    onUpdate: (data: T) => void = () => {}
 ) {
     type OnDataUpdateFunctionType = (data: T) => void;
     const navigate = useNavigate();
-    const [data, setData] = useState<T>();
+    const [data, setData] = useState<T>(defaultValue);
     const onUpdateRef = useRef<OnDataUpdateFunctionType>(() => {});
     const responseFunctionsRef = useRef<StatusResponseFunctions>({});
 
@@ -30,7 +33,10 @@ export default function useAPIResource<T>(
             404: () => {
                 navigate(-1);
             },
+            ...responseFunctions,
         });
+
+        setOnUpdate(onUpdate);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
@@ -54,18 +60,20 @@ export default function useAPIResource<T>(
             .then((response): Promise<OutputType<T>> => {
                 let output: Promise<OutputType<T>>;
                 if (response.status === 200) {
-                    output = new Promise<OutputType<T>>((resolve, reject) => {
+                    output = new Promise<OutputType<T>>((resolve) => {
                         response.json().then((value: T) => {
                             resolve({
                                 status: response.status,
-                                json: value,
+                                value: value,
                             });
+
+                            if (200 in responseFunctionsRef.current) {
+                                responseFunctionsRef.current[200](
+                                    response.statusText
+                                );
+                            }
                         });
                     });
-
-                    if (200 in responseFunctionsRef.current) {
-                        responseFunctionsRef.current[200](response.statusText);
-                    }
                 } else {
                     if (response.status in responseFunctionsRef.current) {
                         responseFunctionsRef.current[response.status](
@@ -81,7 +89,7 @@ export default function useAPIResource<T>(
                 return output;
             })
             .then((output) => {
-                setData(output.json);
+                setData(output.value);
             })
             .catch((statusText) => {
                 console.error(statusText);
