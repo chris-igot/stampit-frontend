1ab6d4e9c36777ed65d73fa4091ecd2ac001b203
cigot <c.bensi.igot@gmail.com>
2022-03-06 20:52:31 -0500

    Fixed and simplified menu item highlight

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -9,6 +9,11 @@ import home from "../icons/round_home_white_48dp.png";
 import people from "../icons/round_people_white_48dp.png";
 import search from "../icons/round_search_white_48dp.png";
 import logout from "../icons/round_logout_white_48dp.png";
+
+const MENU_ITEM_COUNT = 6;
+const MIDDLE_SPACE = 100 / MENU_ITEM_COUNT;
+const END_SPACE = MIDDLE_SPACE / 2;
+
 interface PropsType {
     children?: React.ReactNode;
 }
@@ -38,10 +43,13 @@ function Menu(props: PropsType) {
         ) as HTMLAnchorElement;
 
         if (menuElem) {
+            const width = menuElem.clientWidth / 2;
+            const pos = END_SPACE + menuElem.tabIndex * MIDDLE_SPACE;
             const hightlight = highlightRef.current as HTMLDivElement;
+
             hightlight.style.width = menuElem.clientWidth + "px";
             hightlight.style.height = menuElem.clientHeight + "px";
-            hightlight.style.left = menuElem.offsetLeft + "px";
+            hightlight.style.left = `calc(${pos}% - ${width}px)`;
         }
     }
 
@@ -51,36 +59,42 @@ function Menu(props: PropsType) {
             <NavLink
                 className={({ isActive }) => (isActive ? "picked" : " shadow")}
                 to="/home"
+                tabIndex={0}
             >
                 <Image className="image--menu z--9002" image={home} />
             </NavLink>
             <NavLink
                 className={({ isActive }) => (isActive ? "picked" : " shadow")}
                 to="/search"
+                tabIndex={1}
             >
                 <Image className="image--menu z--9002" image={search} />
             </NavLink>
             <NavLink
                 className={({ isActive }) => (isActive ? "picked" : " shadow")}
                 to="/feed"
+                tabIndex={2}
             >
                 <Image className="image--menu z--9002" image={feed} />
             </NavLink>
             <NavLink
                 className={({ isActive }) => (isActive ? "picked" : " shadow")}
                 to="/public"
+                tabIndex={3}
             >
                 <Image className="image--menu z--9002" image={world} />
             </NavLink>
             <NavLink
                 className={({ isActive }) => (isActive ? "picked" : " shadow")}
                 to="/following"
+                tabIndex={4}
             >
                 <Image className="image--menu z--9002" image={people} />
             </NavLink>
             <Link
                 className="shadow"
                 to="/login"
+                tabIndex={5}
                 onClick={() => {
                     fetch("/api/logout").then(() => {
                         navigate("/login");
